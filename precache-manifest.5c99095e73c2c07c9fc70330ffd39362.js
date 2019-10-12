f5b46db7ae0bfaf226af77e631972c20f84230cb
antuan159 <lanos159@ukr.net>
2019-10-12 20:37:26 +0300

    Updates

---
+++ precache-manifest.5c99095e73c2c07c9fc70330ffd39362.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "587f3f9988051b6a3e1cdcf784a58929",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "553815887b2786fc399a",
+    "url": "/goit-react-hw-01-components/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "0908602a1c95e3fc1987",
+    "url": "/goit-react-hw-01-components/static/css/main.3afc1858.chunk.css"
+  },
+  {
+    "revision": "553815887b2786fc399a",
+    "url": "/goit-react-hw-01-components/static/js/2.ccbe9a95.chunk.js"
+  },
+  {
+    "revision": "0908602a1c95e3fc1987",
+    "url": "/goit-react-hw-01-components/static/js/main.2376070b.chunk.js"
+  },
+  {
+    "revision": "4d9ce36fabe79d32c0ed",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.7a5c0bc9.js"
+  }
+]);
